ac6adfa8180755f2b5f3857b932b32d073f9c641
Vahid <vahid.vaezian@gmail.com>
2020-10-03 12:25:43 -0700

    Update rds_queries.sql

--- rds_queries.sql
+++ rds_queries.sql
@@ -1,22 +1,25 @@
+drop table if exists basics;
 create table basics (
 	id varchar(15),
 	title_type varchar(30),
-	primary_title varchar(200),
-	original_title varchar(200),
-	is_adult smallint,
+	primary_title varchar(500),
+	original_title varchar(500),
+	is_adult int,
 	start_Year char(4),
 	end_year char(4),
-	runtime_minutes smallint,
+	runtime_minutes int,
 	genres varchar(200)
 )
 
+drop table if exists episodes;
 create table episodes (
 	id varchar(15),
 	parent_id varchar(15),
-	season_number smallint,
-	episode_number smallint
+	season_number int,
+	episode_number int
 )
 
+drop table if exists ratings;
 create table ratings (
 	id varchar(15),
 	avg_rating numeric(3, 1),
