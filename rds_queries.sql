5f10f78e00fba6e5d98d92d62bd316eeb91db756
Vahid <vahid.vaezian@gmail.com>
2020-10-03 18:58:11 -0700

    Update rds_queries.sql

--- rds_queries.sql
+++ rds_queries.sql
@@ -32,7 +32,7 @@ create index idx_id_episodes on episodes (id);
 create index idx_parent_id_episodes on episodes (parent_id);
 create index idx_id_ratings on ratings (id);
 
--- creating series table
+-- SERIES
 select a.parent_id "Series ID", a.id "Episode ID", a.season_number "Season", a.episode_number "Episode", b.avg_rating "Rating", b.num_votes "# Votes"
 	, c.original_title, primary_title, c.start_year, c.end_year, runtime_minutes, genres
 into series
@@ -42,6 +42,13 @@ from episodes a
 
 CREATE INDEX idx_original_title_series ON series (LOWER(original_title) varchar_pattern_ops);
 create index idx_original_title_series2 on series (original_title);
+
+-- MOVIES
+select a.id, title_type, original_title, primary_title, start_year, runtime_minutes, genres, avg_rating, num_votes
+into movies
+from basics a
+join ratings b on a.id = b.id
+where title_type in ('movie', 'tvMovie')
 
 -- there are two rows for every record. we delete of them
 -- decided to delete the duplicates on the csv file, because it was more reliable
